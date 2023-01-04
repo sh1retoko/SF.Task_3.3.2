407340cc0c091ad42e0c8fa81c1e43432cc15daa
sh1retoko <120920943+sh1retoko@users.noreply.github.com>
2023-01-04 18:28:53 +0300

    Add files via upload

---
+++ Mod3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Mod3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Name = "Jane";
+            Console.WriteLine($"My name is {Name}");
+
+            int age = 27;
+            Console.WriteLine($"My age is {age}");
+
+            bool pet = true;
+            Console.WriteLine($"Do I have a pet? {pet}");
+
+            double leg = 37.5;
+            Console.WriteLine($"My shoe size is {leg}");
+
+            Console.ReadKey();
+        }
+    }
+}
